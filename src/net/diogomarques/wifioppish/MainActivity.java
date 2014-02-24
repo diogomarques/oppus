27147ff3afc7ba76b7942499e61f7f7207b961ed
André Silva <asilva_sc@hotmail.com>
2014-02-24 17:14:16 +0000

    Moved MessageDumper and TextLog to Loggings package

--- src/net/diogomarques/wifioppish/MainActivity.java
+++ src/net/diogomarques/wifioppish/MainActivity.java
@@ -6,6 +6,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import net.diogomarques.wifioppish.logging.TextLog;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
